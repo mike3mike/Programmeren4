6dee27fee848275a3167207db57e1ca184810b82
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-05-12 13:31:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const userRouter = require('./routes/user.routes');
 app.use("/api", userRouter);
