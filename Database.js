f42da79c5bf2e329b121456849f3e02113d8394d
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-05-12 13:37:23 +0200

    Changed database variable names

--- Database.js
+++ Database.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const pool = conn.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
-    database: process.env.DB_DATABASENAME || 'share_a_meal',
+    database: process.env.DB_DATABASE || 'share_a_meal',
     password: process.env.DB_PASSWORD || 'root',
+    port: process.env.port || '3306',
     waitForConnections: true,
     multipleStatements: true,
     connectionLimit: 10,
