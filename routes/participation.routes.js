3a376e71f27453d8c72429915da4e1b1bc70f170
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-05 20:28:48 +0200

    Test logging req body

--- routes/participation.routes.js
+++ routes/participation.routes.js
@@ -4,7 +4,9 @@ const participationController = require('../controllers/participation.controller
 const router = express.Router();
 
 // UC-401
-router.post("/api/meal/:mealId/participate", authController.validate, participationController.register)
+router.post("/meal/:mealId/participate", authController.validate, participationController.register)
 
 // UC-402
 router.delete("/:mealId", participationController.delete)
+
+module.exports = router;
