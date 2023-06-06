70c5dc3813486ea1af9e946bef475660f28c8e35
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-06 05:54:28 +0200

    .

--- index.js
+++ index.js
@@ -18,47 +18,47 @@ app.use((req, res, next) => {
 // const mealRouter = require('./routes/meal.routes');
 // app.use("/api/meal", mealRouter);
 
-app.use((err, req, res, next) => {
-    // if (err.code != undefined) {
-    //     console.log("Error code: " + err.code);
-    // } else {
-    //     console.log(err);
-    // }
-    // console.log(err);
-    if (!res.headersSent) {
-        if (err.status) {
-            res.status(err.status).json(
-                {
-                    status: err.status,
-                    errCode: err.code,
-                    message: err.message,
-                    data: {}
-                }
-            );
-        } else if (err.code || err.message) {
-            res.status(500).json(
-                {
-                    status: 500,
-                    errCode: err.code,
-                    message: err.message,
-                    data: {}
-                }
-            );
-        } else {
-            res.status(500).json({
-                status: 500,
-                message: "",
-                data: {}
-            });
-        }
-    }
-})
+// app.use((err, req, res, next) => {
+//     // if (err.code != undefined) {
+//     //     console.log("Error code: " + err.code);
+//     // } else {
+//     //     console.log(err);
+//     // }
+//     // console.log(err);
+//     if (!res.headersSent) {
+//         if (err.status) {
+//             res.status(err.status).json(
+//                 {
+//                     status: err.status,
+//                     errCode: err.code,
+//                     message: err.message,
+//                     data: {}
+//                 }
+//             );
+//         } else if (err.code || err.message) {
+//             res.status(500).json(
+//                 {
+//                     status: 500,
+//                     errCode: err.code,
+//                     message: err.message,
+//                     data: {}
+//                 }
+//             );
+//         } else {
+//             res.status(500).json({
+//                 status: 500,
+//                 message: "",
+//                 data: {}
+//             });
+//         }
+//     }
+// })
 
-app.use((req, res) => {
-    console.log(req.originalUrl);
-    console.log("Status Code: 404");
-    res.status(404).end();
-})
+// app.use((req, res) => {
+//     console.log(req.originalUrl);
+//     console.log("Status Code: 404");
+//     res.status(404).end();
+// })
 
 app.listen(port, () => {
     console.log(`Application is running on ` + port + '.')
