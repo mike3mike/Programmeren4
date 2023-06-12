840e6b015e0999629368457338a8b1e8aed0bd1c
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-12 22:45:50 +0200

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,17 +14,16 @@ router.use('/user', userRouter);
 // UC-201
 userRouter.post('', userController.registerUser)
 // UC-202
-// userRouter.get('', userController.getUserList)
+userRouter.get('', userController.getUserList)
 
-// // UC-203
-// userRouter.get("/profile", authController.validate, userController.getProfile)
+// UC-203
+userRouter.get("/profile", authController.validate, userController.getProfile)
 
-// // UC-204
-// userRouter.get("/:userId", authController.validate, userController.getProfileById)
-// // userRouter.get("/:userId", userController.getProfileById)
-// // UC-205
-// userRouter.put("/:userId", authController.login, authController.validate, userController.updateUser)
-// // UC-206
+// UC-204
+userRouter.get("/:userId", authController.validate, userController.getProfileById)
+// UC-205
+userRouter.put("/:userId", authController.login, authController.validate, userController.updateUser)
+// UC-206
 userRouter.delete("/:userId", authController.validate, userController.deleteUser)
 
 
