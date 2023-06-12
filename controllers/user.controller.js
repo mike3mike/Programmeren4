820b37f7ffbba27b8ab9a988c0202147ade006b8
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-12 20:51:16 +0200

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -106,7 +106,7 @@ const userController = {
                         correctParameters.forEach((i, index) => {
                             replaceDict = {"true":"1", "false":"2"};
                             replacedValue = i[1].replace("true", 1).replace("false", "0");
-                            whereQuery += (i[0] + " = " + "'" + i[1] + "'");
+                            whereQuery += (i[0] + " = " + "'" + replacedValue + "'");
                             if (index != correctParameters.length - 1) {
                                 whereQuery += " AND ";
                             }
