fd8f6ee6dddbdab2630efd597cf584c767896d71
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-12 22:03:44 +0200

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,23 +9,23 @@ router.post('/login', authController.login, userController.login)
 router.get('/info', userController.info)
 
 const userRouter = express.Router();
-// router.use('/user', userRouter);
+router.use('/user', userRouter);
 
 // UC-201
 userRouter.post('', userController.registerUser)
 // UC-202
-userRouter.get('', userController.getUserList)
+// userRouter.get('', userController.getUserList)
 
-// UC-203
-userRouter.get("/profile", authController.validate, userController.getProfile)
+// // UC-203
+// userRouter.get("/profile", authController.validate, userController.getProfile)
 
-// UC-204
-userRouter.get("/:userId", authController.validate, userController.getProfileById)
-// userRouter.get("/:userId", userController.getProfileById)
-// UC-205
-userRouter.put("/:userId", authController.login, authController.validate, userController.updateUser)
-// UC-206
-userRouter.delete("/:userId", authController.validate, userController.deleteUser)
+// // UC-204
+// userRouter.get("/:userId", authController.validate, userController.getProfileById)
+// // userRouter.get("/:userId", userController.getProfileById)
+// // UC-205
+// userRouter.put("/:userId", authController.login, authController.validate, userController.updateUser)
+// // UC-206
+// userRouter.delete("/:userId", authController.validate, userController.deleteUser)
 
 
 module.exports = userRouter;
