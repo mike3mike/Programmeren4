1abce0f8061cd609164dd8ff7005d3f4a10f79e7
mike3mike <51219765+mike3mike@users.noreply.github.com>
2023-06-12 21:10:31 +0200

    Update meal.controller.js

--- controllers/meal.controller.js
+++ controllers/meal.controller.js
@@ -253,8 +253,8 @@ const participationController = {
                             error.status = 403
                             next(error);
                         } else {
-                            conn.query("SELECT COUNT(*) AS participantCount FROM meal m WHERE m.id = ?", [req.params.mealId], function (queryError, participantCount, fields) {
-                                if (participantCount.participantCount == 0) {
+                            conn.query("SELECT COUNT(*) AS participantCount FROM meal_participants_user up WHERE up.mealId = ?", [req.params.mealId], function (queryError, participantCount, fields) {
+                                if (participantCount.participantCount == 1 && results.affectedRows == 1) {
                                     res.status(200).json({
                                         status: 404,
                                         message: "Meal does not exist. User met ID " + req.user.id + " is niet aangemeld voor maaltijd met ID " + req.params.mealId,
